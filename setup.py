4154f92b187b00c2650c06ff973d8b3a74acbfe8
jartigag <jartigag@pm.me>
2021-05-08 14:39:40 +0200

    find blunders in matches

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='analyzing the most common chess blunders',
+    author='@jartigag',
+    license='',
+)
