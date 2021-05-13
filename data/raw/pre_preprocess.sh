667430494c0f96d07f83bf2698728e995c5e198b
jartigag <jartigag@pm.me>
2021-05-13 09:36:57 +0200

    grep --no-group-separator

--- data/raw/pre_preprocess.sh
+++ data/raw/pre_preprocess.sh
@@ -6,7 +6,7 @@ filename="${1%.*}"
 if [ $extension == "bz2" ]
 then
     #bunzip2 --keep $1
-    bzcat $1 | grep -B18 "%eval" > "$filename.eval.pgn"
+    bzcat $1 | grep -B18 -A1 --no-group-separator "%eval" > "$filename.eval.pgn"
 else
-    grep -B18 "%eval" $filename.pgn > "$filename.eval.pgn"
+    grep -B18 -A1 --no-group-separator "%eval" $filename.pgn > "$filename.eval.pgn"
 fi
