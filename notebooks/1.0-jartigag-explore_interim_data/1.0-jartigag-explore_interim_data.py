3c1146a0238310e06eb2ab2fa86c7ba4c353ada5
jartigag <jartigag@pm.me>
2021-05-27 00:10:57 +0200

    looks like more data needs to be processed

--- notebooks/1.0-jartigag-explore_interim_data/1.0-jartigag-explore_interim_data.py
+++ notebooks/1.0-jartigag-explore_interim_data/1.0-jartigag-explore_interim_data.py
@@ -13,5 +13,6 @@ grouped_by_elo = df_data.groupby( pd.cut(df_data['Elo'], np.arange(1400, 3000, 2
 grouped_by_elo.size()
 
 
-grouped_by_elo.groups.keys()
+vc = grouped_by_elo.get_group(pd.Interval(1400, 1600, closed='right')).value_counts(['Move', 'FEN'])
+vc[vc>1]
 
