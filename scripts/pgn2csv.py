cf23a6872aa3ea45c0c67ca78248d9ab69a9a202
jartigag <jartigag@pm.me>
2021-11-22 10:36:34 +0100

    pgn2csv.py: exception handling

--- scripts/pgn2csv.py
+++ scripts/pgn2csv.py
@@ -10,15 +10,19 @@ import sys
 import signal
 
 
-def sigint_handler(signal, frame):
+def print_status_info():
     global written_lines
     print(file=sys.stderr)
     print("last read date-time:",dic['UTCDate'],dic['UTCTime'],file=sys.stderr)
     print("last read players:  ",dic['White'],dic['Black']    ,file=sys.stderr)
     print("last read line:     ",line                         ,file=sys.stderr)
-    print("read lines:    ",numline                           ,file=sys.stderr)
-    print("written lines: ",written_lines                     ,file=sys.stderr)
+    print(f"read lines:    {numline:,}"                       ,file=sys.stderr)
+    print(f"written lines: {written_lines:,}"                 ,file=sys.stderr)
     sys.exit(-1)
+
+
+def sigint_handler(signal, frame):
+    print_status_info()
 
 
 if __name__ == '__main__':
@@ -37,18 +41,24 @@ if __name__ == '__main__':
         writer = csv.DictWriter(wf, fieldnames=dic_keys)
         writer.writeheader()
         for numline,line in enumerate(f):
-            if line[0] == "[":
-                keyvalue = line[1:-1]
-                key = keyvalue.split()[0]
-                dic[key] = keyvalue[len(key):-2].strip().strip('"')
-            if line[0] == "1":
-                dic["Game"] = line.rstrip('\n')
-                if dic['UTCDate']==None:
-                    print("\033[0;31m[-]\033[0m game without header (no metadata), so it will be discarded:", line, file=sys.stdout) # to count them, ./pgn2csv.py input.pgn | wc -l
-                    continue
-                dic['Date'] = dic['UTCDate']+" "+dic['UTCTime']
-                writer.writerow(dic)
-                written_lines += 1
-                dic = {k: None for k in dic_keys}
-            else:
-                pass
+            try:
+                if line[0] == "[":
+                    keyvalue = line[1:-1]
+                    key = keyvalue.split()[0]
+                    dic[key] = keyvalue[len(key):-2].strip().strip('"')
+                if line[0] == "1":
+                    dic["Game"] = line.rstrip('\n')
+                    if dic['UTCDate']==None:
+                        print("\033[0;31m[-]\033[0m game without header (no metadata), so it will be discarded:", line, file=sys.stdout) # to count them, ./pgn2csv.py input.pgn | wc -l
+                        continue
+                    dic['Date'] = dic['UTCDate']+" "+dic['UTCTime']
+                    writer.writerow(dic)
+                    written_lines += 1
+                    dic = {k: None for k in dic_keys}
+                else:
+                    pass
+            except Exception as e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = exc_tb.tb_frame.f_code.co_filename
+                print(f'File "{fname}", line {exc_tb.tb_lineno}\n{type(e).__name__}: {e}', file=sys.stderr)
+                print_status_info()
